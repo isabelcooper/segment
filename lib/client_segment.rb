23c9b55261cc8581886ace1314f628ccc112f113
Isabel Cooper <Isabel@MacBook-Pro-8.local>
2018-10-03 12:33:12 +0100

    small classifed

--- lib/client_segment.rb
+++ lib/client_segment.rb
@@ -1,7 +1,14 @@
 def client_segment(client)
-   "small"
+   if (client.employees < 50 && client.offices == 1 && client.turnover_in_millions < 1.0 )
+     "small"
+     else "enterprise"
+   end
 end
 
 def segment(clients)
 
 end
+
+#  Small - employee count is less than 50 and number of offices is one and annual turnover is less than £1M.
+# Enterprise - all other clients.
+
--- spec/client_segment_spec.rb
+++ spec/client_segment_spec.rb
@@ -3,8 +3,19 @@ require "./lib/client_segment"
 
 describe "client_segment" do
   it "classifies small clients" do
-    small_client = Client.new(1,1,0.1)
-    segment = client_segment(small_client)
-    expect(segment).to eq"small"
+    expect(client_segment(Client.new(1, 1, 0.1))).to eq "small"
+    expect(client_segment(Client.new(50, 1, 0.1))).to eq "enterprise"
+    expect(client_segment(Client.new(1, 2, 0.1))).to eq "enterprise"
+    expect(client_segment(Client.new(1, 1, 1.0))).to eq "enterprise"
   end
 end
+
+
+=begin
+ Step 1
+
+Can you write a program that returns one of two classifications given a client. The classifications are:
+
+Small - employee count is less than 50 and number of offices is one and annual turnover is less than £1M.
+Enterprise - all other clients.
+=end
